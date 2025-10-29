3eb9c111e31731384325eb3efac2868b470f4979
ashish <ashishdede05@gmail.com>
2025-10-29 09:57:02 +0530

    das till stack

--- DSA/dsa.py
+++ DSA/dsa.py
@@ -1,2 +1,8 @@
 # DSA : Data structures are the way of storing and orgnizing data in computer
 # built-in support for several data structures list,dictionaries,sets 
+# Python list and arrays - 
+# In python list and arrays are the built-in data structure that serves a dynamic array
+# list are ordered, mutuble and can contain elements of different types
+# Time Complexity -
+# time complexity is the number of operations needed to run an algorithm for large amount of data
+# and the number of operations can be considered as time because the computer uses some time for each operation 
---
+++ DSA/stacks.py
@@ -0,0 +1,18 @@
+# Stacks in python :
+# Stack is the linear data structure that follows the LAST IN FIRST OUT (LIFO)
+# statck is the data structure that can hold many elements, and the last element added is the fist one to be removed
+# Operations on stack : 
+# 1) push : adds a new element on the stack
+# 2) pop : Removes and returns the top element from the stack
+# 3) peek : Returns the top (last) element on the stack
+# 4) isEmpty : Checks if the stack is empty
+# 5) size : Finds the number of elements in the stack
+# stacks can be implemented using arrays or linked lists
+stack = []
+stack.append('A')
+stack.append('B')
+stack.append('C')
+print(f"stack : {stack}")
+
+topElement = stack[-1]
+print(f"peek :")
